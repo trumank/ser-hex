6da9168556f3f742ff17fd6aea189e4fa168215d
Truman Kilen <t@kilen.me>
2024-06-19 19:15:33 -0500

    Hide console on windows

--- ser-hex-viewer/src/main.rs
+++ ser-hex-viewer/src/main.rs
@@ -1,3 +1,5 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+
 use std::{
     collections::hash_map::DefaultHasher,
     ops::{Range, RangeBounds},
